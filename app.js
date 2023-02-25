c3d4fd4278f98b504fec2efc20e0cea58f4c8ca0
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2023-02-25 15:16:52 +0100

    #ajout commentaires

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
+// déclarer le résultat du lancer du dé
 let result;
 
+// Lancer le dé
 let rollonBtn  = () => {
   let roll = Math.floor(Math.random()*6+1);
 return roll;
 };
 
+
 let rollOn = ()=>{
+  // Récupérer le résultat du lancer
   result  = rollonBtn();
+
+  // Récupérer le bouton currentplayer1
   let currentplayer1 = document.getElementById('currentplayer1');
+  
+  // Ajouter du texte à currentplayer1
   currentplayer1.textContent = result;
 
+  //  Récupérer le bouton d'affichage du dé
   let displayDice = document.getElementById('dicedisplay');
+  
+    // Afficher le dé
   displayDice.src = 'img/dice' + result + '.png';
-
-let hold = document.getElementById('hold');
-hold.addEventListener('click' , () =>{
-  let roundPlayer1 = document.getElementById('roundplayer1');
-  roundPlayer1.textContent = result + result;
-})
 }
 
+//  Récupérer le bouton lancer le dé
 let rollDice = document.getElementById('rolldice');
+
+// Exécuter le lancer du dé lors du click
 rollDice.addEventListener('click', rollOn)
+
+let boub = () =>{
+  //  Récupérer le bouton roundplayer1
+  let roundPlayer1 = document.getElementById('roundplayer1');
+  
+  // Ajouter du texte à roundplayer1
+  roundPlayer1.textContent = result + result;
+};
+
+//  Récupérer le bouton HOLD
+let hold = document.getElementById('hold');
+
+// Exécuter le bouton HOLD lors du click
+hold.addEventListener('click', boub);
 
 
 
