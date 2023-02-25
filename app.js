f6ac1f7f126fe1410970e6413e084e19594833cb
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2023-02-25 15:02:09 +0100

    #réorganisation du code dans app.js

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
 let result;
-let rollDice = document.getElementById('rolldice');
-rollDice.addEventListener('click', () =>{
-    
-  let rollon  = () => {
-    let roll = Math.floor(Math.random()*6+1);
-  return roll;
+
+let rollonBtn  = () => {
+  let roll = Math.floor(Math.random()*6+1);
+return roll;
 };
 
-  result  = rollon();
+let rollOn = ()=>{
+  result  = rollonBtn();
+  let currentplayer1 = document.getElementById('currentplayer1');
+  currentplayer1.textContent = result;
 
   let displayDice = document.getElementById('dicedisplay');
   displayDice.src = 'img/dice' + result + '.png';
-  
-   let currentPlayer1 = document.getElementById('currentplayer1');
-  currentPlayer1.textContent = result;
-
-    // affichage resultat du score global joueur 1
-  
-});
 
 let hold = document.getElementById('hold');
 hold.addEventListener('click' , () =>{
   let roundPlayer1 = document.getElementById('roundplayer1');
   roundPlayer1.textContent = result + result;
-
 })
+}
+
+let rollDice = document.getElementById('rolldice');
+rollDice.addEventListener('click', rollOn)
+
 
 
 
