5d8783fa45b8da9a0e101960c4fab48c1744f74a
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2023-02-25 19:09:17 +0100

    #regroupement des boutons écoutés

--- app.js
+++ app.js
@@ -7,33 +7,31 @@ let rollonBtn  = () => {
 return roll;
 };
 
+ // Récupération des boutons
+let currentplayer1 = document.getElementById('currentplayer1');
+let currentplayer2 = document.getElementById('currentplayer2');
+let displayDice = document.getElementById('dicedisplay');
+let rollDice = document.getElementById('rolldice');
+let roundPlayer1 = document.getElementById('roundplayer1');
+let newgame = document.getElementById('newgame');
 
 let rollOn = ()=>{
   // Récupérer le résultat du lancer
   result  = rollonBtn();
-
-  // Récupérer le bouton currentplayer1
-  let currentplayer1 = document.getElementById('currentplayer1');
-  
+    
   // Ajouter du texte à currentplayer1
   currentplayer1.textContent = result;
 
-  //  Récupérer le bouton d'affichage du dé
-  let displayDice = document.getElementById('dicedisplay');
-  
     // Afficher le dé
   displayDice.src = 'img/dice' + result + '.png';
 }
 
-//  Récupérer le bouton lancer le dé
-let rollDice = document.getElementById('rolldice');
-
 // Exécuter le lancer du dé lors du click
 rollDice.addEventListener('click', rollOn)
 
-let boub = () =>{
-  //  Récupérer le bouton roundplayer1
-  let roundPlayer1 = document.getElementById('roundplayer1');
+
+// initialiser le bouton roundplayer1
+let holder = () =>{
   
   // Ajouter du texte à roundplayer1
   roundPlayer1.textContent = result + result;
@@ -43,13 +41,11 @@ let boub = () =>{
 let hold = document.getElementById('hold');
 
 // Exécuter le bouton HOLD lors du click
-hold.addEventListener('click', boub);
-
-// Récupérer le bouton newgame
-let newgame = document.getElementById('newgame');
+hold.addEventListener('click', holder);
 
 let newGamer = () => {
   currentplayer1.textContent = 0;
+  currentplayer2.textContent = 0;
 }
 
 // Exécuter le bouton Newgame lors du click
