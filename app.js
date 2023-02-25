ce5262f59564b6c5e568f5c71faadcbd6883f8ff
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2023-02-25 07:29:43 +0100

    #affichage du dé dans le navigateur

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 let rollDice = document.getElementById('rolldice');
 rollDice.addEventListener('click', () =>{
-  let rollon  = Math.floor(Math.random()*6+1);
-  return rollon; 
+    
+  let rollon  = () => {
+    let roll = Math.floor(Math.random()*6+1);
+  return roll;
+
+};
+
+const diceImg = {
+    '1' : "<img src=./img/dice1.png\>" ,
+    '2' : "<img src=./img/dice2.png\>",
+    '3' : "<img src=./img/dice3.png\>",
+    '4' : "<img src=./img/dice4.png\>",
+    '5' : "<img src=./img/dice5.png\>",
+    '6' : "<img src=./img/dice6.png\>",
+  };
+
+  const result = rollon();
+
+  let displayDice = document.getElementById('dicedisplay')
+  displayDice.innerHTML = (diceImg[result]);
+
+  console.log(result);
+});
+
+
   
-})
