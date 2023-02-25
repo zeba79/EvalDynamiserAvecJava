fd2aedb43409b3199f6a7d43cd39e1d20be26823
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2023-02-25 07:48:42 +0100

    #stylisation de dicedisplay dans style.css + ajout with et height app.js

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ rollDice.addEventListener('click', () =>{
   let rollon  = () => {
     let roll = Math.floor(Math.random()*6+1);
   return roll;
-
 };
 
 const diceImg = {
-    '1' : "<img src=./img/dice1.png\>" ,
-    '2' : "<img src=./img/dice2.png\>",
-    '3' : "<img src=./img/dice3.png\>",
-    '4' : "<img src=./img/dice4.png\>",
-    '5' : "<img src=./img/dice5.png\>",
-    '6' : "<img src=./img/dice6.png\>",
+    '2' : "<img src=./img/dice2.png \ width=\"80\" height=\"80\" \>",
+    '3' : "<img src=./img/dice3.png \ width=\"80\" height=\"80\" \>",
+    '4' : "<img src=./img/dice4.png \ width=\"80\" height=\"80\" \>",
+    '1' : "<img src=./img/dice1.png \ width=\"80\" height=\"80\" \>" ,
+    '5' : "<img src=./img/dice5.png \ width=\"80\" height=\"80\" \>",
+    '6' : "<img src=./img/dice6.png \ width=\"80\" height=\"80\" \>",
   };
 
   const result = rollon();
